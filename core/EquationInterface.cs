b5378e927abee6a2367fb2c28bfb9d99c9c33e76
SukaSharagaZaebala <tsibinegor@yandex.ru>
2019-12-18 16:58:04 +0300

    BLYAT
    Lab 8 Egor Cerber

---
+++ core/EquationInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab.core
+{
+    interface EquationInterface
+    {
+        List<float> Solve(float a, float b, float c);
+    }
+}
