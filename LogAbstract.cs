f83addb1dd925a58cdd7db48f80ddda14250308b
SukaSharagaZaebala <tsibinegor@yandex.ru>
2019-12-18 17:08:42 +0300

    Цибин 8 lab

---
+++ LogAbstract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab.core
+{
+    abstract class LogAbstract<T>
+        where T:LogAbstract<T>, new()
+    {
+        protected static T i = new T();
+
+        public static T Instance()
+        {
+            return i;
+        }
+
+        protected List<string> log = new List<string>();
+
+        abstract public void _write();
+    }
+}
